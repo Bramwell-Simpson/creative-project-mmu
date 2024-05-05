42516adcd89178881b7ea33950f8f299dfda8f59
Bramwell Simpson <bramwellsimpson1@gmail.com>
2024-05-05 04:10:34 +0100

    Add tutorial scene boilerplates

---
+++ src/scenes/tutorials/tutorialLevel01.js
@@ -0,0 +1,6 @@
+import Phaser from "phaser";
+
+export default class TutorialLevel01 extends Phaser.Scene
+{
+    
+}
---
+++ src/scenes/tutorials/tutorialLevel03.js
@@ -0,0 +1,6 @@
+import Phaser from "phaser";
+
+export default class TutorialLevel03 extends Phaser.Scene
+{
+    
+}
