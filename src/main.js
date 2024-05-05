43634c114050913a72669d4a03982e744f837785
Bramwell Simpson <bramwellsimpson1@gmail.com>
2024-05-05 06:03:50 +0100

    Add scenes to config

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Phaser from 'phaser'
 
 import Level01Scene from './scenes/Level01Scene'
 import Level02Scene from './scenes/Level02Scene'
-import TutorialScene from './scenes/TutorialScene'
+import TutorialLevel01 from './scenes/tutorials/tutorialLevel01'
+import TutorialLevel02 from './scenes/tutorials/tutorialLevel02'
+import TutorialLevel03 from './scenes/tutorials/tutorialLevel03'
+import TutorialLevel04 from './scenes/tutorials/tutorialLevel04'
 
 const config = {
 	type: Phaser.AUTO,
@@ -15,7 +18,7 @@ const config = {
 
     pixelArt: true,
     backgroundColor: '#00000',
-	scene: [TutorialScene, Level01Scene, Level02Scene]
+	scene: [TutorialLevel01, TutorialLevel02, TutorialLevel03, TutorialLevel04, Level01Scene, Level02Scene]
 }
 
 export default new Phaser.Game(config)
